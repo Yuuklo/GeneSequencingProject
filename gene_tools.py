59a10ce3c28f3363620d24ca5ea94f2fc2020382
Calvin Wang <cwang3350@gmail.com>
2025-02-26 22:09:01 -0500

    gene_tools commit
    Didn't commit this portion of my code from earlier.

--- gene_tools.py
+++ gene_tools.py
@@ -1,5 +1,5 @@
-from Bio.Seq import Seq
-from Bio.SeqUtils import CodonUsage
+import pandas as pd
+import codon_chart as cc
 
 # Returns the complement strand of a DNA sequence
 def to_complement(sequence):
@@ -11,18 +11,37 @@ def to_complement(sequence):
 def transcribe(sequence):
 	return sequence.replace('T','U')
 
-# Translates a mRNA sequence into their corresponding amino acid structure with the help the *biopython* library
+# Translates a mRNA sequence into their corresponding amino acid structure
 def translate(sequence):
-	three_letter_aminos = {
-	    'A': 'Ala', 'R': 'Arg', 'N': 'Asn', 'D': 'Asp', 'C': 'Cys',
-	    'Q': 'Gln', 'E': 'Glu', 'G': 'Gly', 'H': 'His', 'I': 'Ile',
-	    'L': 'Leu', 'K': 'Lys', 'M': 'Met', 'F': 'Phe', 'P': 'Pro',
-	    'S': 'Ser', 'T': 'Thr', 'W': 'Trp', 'Y': 'Tyr', 'V': 'Val', '*': 'Stop'
-	}
-	translated = Seq(sequence).translate() # Returns the single-letter key values of each amino acid
-	three_letter_translation = ' '.join(three_letter_aminos[i] for i in translated) # Iterates through each letter and gives it a corresponding 3-Letter Amino Acid
-	return three_letter_translation
-	
+	codons = [sequence[i:i+3] for i in range(0, len(sequence), 3)] # Traversing the mRNA sequence to find Codons in threes
+	codon_chart = cc.table
+	amino_acids = []
+	for codon in codons:
+		for amino_acid, codon_list in codon_chart.items():
+			if codon in codon_list:
+				amino_acids.append(amino_acid)
+	return to_three_letter_acids(amino_acids)
+
+# Iterates through each letter and appends it's corresponding 3-letter amino acid name
+def to_three_letter_acids(amino_acids):
+	amino_chart = cc.amino_table
+	amino_acid_sequence = []
+	for acid in amino_acids:
+		if acid in amino_chart:
+			if acid == '*':
+				pass # STOP Codon does not get printed
+			else:
+				amino_acid_sequence.append(amino_chart[acid])
+	return '-'.join(amino_acid_sequence)
+
+
+
+# Reads a translated amino acid sequence and trims off the non-coding proteins
+def find_coding_regions(translation):
+	# The translate() function built inside the biopython function denotes the letter 'M' as a START CODON and an asterisk '*' as the STOP CODON.
+	# Therefore, finding the coding regions of a translated sequence will involve trimming based off the indices of M and *.
+	coding_regions = [] 
+	...
 
 # Simulate different types of mutations 
 def mutate():
